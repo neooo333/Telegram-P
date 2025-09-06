c0ee54e02a4acfec408a1cfba34f1b5286d2e799
neooo333 <nikita.puzevich@gmail.com>
2025-09-05 21:42:50 -0400

    Update 9/5/2025

--- Telegram.py
+++ Telegram.py
@@ -9,6 +9,7 @@ from telegram.ext import (
 )
 import finance_functions as ff
 
+
 with open('telegram_key.txt', 'r') as key:
     key_file = key.read ().strip ()
 
@@ -20,22 +21,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-"""
-1. /start - send greeting!
-2. /help - calls command menu 
-3. /exchange - calls for exchnge market function for a specified security
-4. /pe_qqq - returns 5 NASDAQ stocks with lowest PE
-""")
+        "1. /start - send greeting!\n"
+        "2. /help - calls command menu\n"
+        "3. /exchange - calls for exchnge market function for a specified security\n"
+        "4. /pe_qqq - returns 5 NASDAQ stocks with lowest PE\n"
+        "4. /pe_snp - returns 5 SNP 500 stocks with lowest PE\n"
+)
 
 async def pe_qqq(update:Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text ('Loading data')
+    await update.message.reply_text ('Loading data...')
     five_stocks = ff.pe_qqq ()
     message = (
         f"1. {five_stocks.index[0]} | PE: {five_stocks.iloc[0]['PE']} | Dividends: {five_stocks.iloc[0]['DIV']}\n"
         f"2. {five_stocks.index[1]} | PE: {five_stocks.iloc[1]['PE']} | Dividends: {five_stocks.iloc[1]['DIV']}\n"
         f"3. {five_stocks.index[2]} | PE: {five_stocks.iloc[2]['PE']} | Dividends: {five_stocks.iloc[2]['DIV']}\n"
-        f"2. {five_stocks.index[3]} | PE: {five_stocks.iloc[3]['PE']} | Dividends: {five_stocks.iloc[3]['DIV']}\n"
-        f"2. {five_stocks.index[4]} | PE: {five_stocks.iloc[4]['PE']} | Dividends: {five_stocks.iloc[4]['DIV']}"
+        f"4. {five_stocks.index[3]} | PE: {five_stocks.iloc[3]['PE']} | Dividends: {five_stocks.iloc[3]['DIV']}\n"
+        f"5. {five_stocks.index[4]} | PE: {five_stocks.iloc[4]['PE']} | Dividends: {five_stocks.iloc[4]['DIV']}"
+    )
+    await update.message.reply_text (message)
+
+async def pe_snp (update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text ('Loading Data...')
+    snp_data_five = ff.pe_snp ()
+    message = (
+        f"1. {snp_data_five.index[0]} | PE: {snp_data_five.iloc[0]['PE']} | Dividends: {snp_data_five.iloc[0]['DIV']}\n"
+        f"2. {snp_data_five.index[1]} | PE: {snp_data_five.iloc[1]['PE']} | Dividends: {snp_data_five.iloc[1]['DIV']}\n"
+        f"3. {snp_data_five.index[2]} | PE: {snp_data_five.iloc[2]['PE']} | Dividends: {snp_data_five.iloc[2]['DIV']}\n"
+        f"4. {snp_data_five.index[3]} | PE: {snp_data_five.iloc[3]['PE']} | Dividends: {snp_data_five.iloc[3]['DIV']}\n"
+        f"5. {snp_data_five.index[4]} | PE: {snp_data_five.iloc[4]['PE']} | Dividends: {snp_data_five.iloc[4]['DIV']}"
     )
     await update.message.reply_text (message)
 
@@ -68,12 +81,6 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
     app = ApplicationBuilder().token(key_file).build()
 
@@ -92,6 +99,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(conv_exchange) 
     app.add_handler(CommandHandler("pe_qqq", pe_qqq))
+    app.add_handler(CommandHandler("pe_snp", pe_snp))
 
     print("Bot is running...")
     app.run_polling()
--- finance_functions.py
+++ finance_functions.py
@@ -8,6 +8,7 @@ with open ("fin_modeling_key.txt", "r") as fin_key:
     fkey = fin_key.read ().strip ()
 
 qqq_tickers = pd.read_csv ("tickers_qqq.csv", header =None)
+snp_tickers = pd.read_csv ("snp500.csv")
 
 
 def exchange_name (company:str):
@@ -19,6 +20,33 @@ def exchange_name (company:str):
 
 def pe_qqq ():
     tick_list = qqq_tickers[0].to_list ()
+    tickers_info = yf.Tickers(tick_list)
+
+    dictionary_pe = dict ()
+
+    for tick in tick_list:
+        try:
+            temp_tick = tickers_info.tickers [tick].info ['trailingPE']
+        except:
+            temp_tick = None
+            print (f"Ticker {tick} doesn't have PE ratio!")
+
+        try:
+            temp_tick_div = tickers_info.tickers [tick].info ['dividendYield']
+        except:
+            temp_tick_div = None
+            print (f"Ticker {tick} doesn't have Dividends!")
+        
+        dictionary_pe[tick] = [temp_tick, temp_tick_div]
+    
+    df_pe = pd.DataFrame (data = dictionary_pe.values (), columns = ["PE", "DIV"],index = dictionary_pe.keys ())
+    df_pe = df_pe.dropna(subset = 'PE', axis=0)
+    df_pe_sorted_head = df_pe.sort_values (by = "PE", ascending = True).head ()
+    return df_pe_sorted_head
+
+
+def pe_snp ():
+    tick_list = snp_tickers['Symbol'].to_list ()
     tickers_info = yf.Tickers(tick_list)
 
     dictionary_pe = dict ()
